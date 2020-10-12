fd7e6cfdfdc77262a0e3b77bbc8d7670a4440a9f
Mário César Silva <mcgato33@hotmail.com>
2020-10-12 16:22:19 -0100

    estilizacao global e landing pages

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoIng from './images/logo.svg';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Oi Mário César</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoIng} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Ilha de Santiago</strong>
+          <span>Praia</span>
+        </div>
+
+        <a href="#" className="enter-app">
+          >
+        </a>
+      </div>
     </div>
   );
 }
