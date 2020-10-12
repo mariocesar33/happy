18ab571f7c14af348473091dcf691b22d730bc0e
Mário César Silva <mcgato33@hotmail.com>
2020-10-12 18:54:46 -0100

    criar arquivo de roura com react-router-dom

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,32 +1,14 @@
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi';
+
+import Routes from './routes';
 
 import './styles/global.css';
-import './styles/pages/landing.css';
-
-import logoIng from './images/logo.svg';
 
 function App() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoIng} alt="Happy" />
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-
-        <div className="location">
-          <strong>Ilha de Santiago</strong>
-          <span>Praia</span>
-        </div>
-
-        <a href="#" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)"/>
-        </a>
-      </div>
-    </div>
+    <>
+      <Routes />
+    </>
   );
 }
 
