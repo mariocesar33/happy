636038955a86f45a8e5c5e42e2d9c076999a8b0c
Mário César Silva <mcgato33@hotmail.com>
2020-10-13 14:11:45 -0100

    add ts-node-dev -D e crescentando script no package.json

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/users', (request, response) => {
+  return response.json({ messgem: "ola mundo"});
+});
+
 app.listen(3333, () => {
   console.log('server start => 3333');
 });
