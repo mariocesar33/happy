66100f8903d1ae16d7534bea62472c1fbdd484a0
Mário César Silva <mcgato33@hotmail.com>
2020-10-13 22:52:31 -0100

    abstrair o codigo

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,39 +1,14 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
 
 import './database/connection';
+import routes from './routes';
 
 const app = express();
+
 app.use(express.json());
+app.use(routes)
 
-app.post('/orphanages', async (request, response) => {
-  const {
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends
-  } = request.body;
 
-  const orphanagesRepository = getRepository(Orphanage);
-
-  const orphanage = orphanagesRepository.create({
-    name,
-    latitude,
-    longitude,
-    about,
-    instructions,
-    opening_hours,
-    open_on_weekends
-  });
-
-  await orphanagesRepository.save(orphanage);
-
-  return response.json({ mensagem: "OI MUNDO"});
-});
 
 app.listen(3333, () => {
   console.log('server start => 3333');
