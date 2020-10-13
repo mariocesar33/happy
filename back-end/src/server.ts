bc4ce6e93ec185d7bc243a5574c6cfe2d1c5670f
Mário César Silva <mcgato33@hotmail.com>
2020-10-13 17:13:26 -0100

    criando e configurando typeorm

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
-
 app.use(express.json());
-
 
 app.post('/users', (request, response) => {
   return response.json({ mensagem: "OI MUNDO"});
