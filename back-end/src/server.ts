c9ba77edc1382ae6d39a44a545dc693f04f43f2c
Mário César Silva <mcgato33@hotmail.com>
2020-10-14 16:11:05 -0100

    excessões e validação de dados

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import path from 'path';
+import 'express-async-errors';
 
 import './database/connection';
+
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
@@ -10,6 +13,7 @@ app.use(express.json());
 app.use(routes)
 
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 
 
