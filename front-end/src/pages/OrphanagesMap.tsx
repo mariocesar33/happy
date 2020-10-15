40c33dafbf54db4e0e801b3aa82cae8d8c805259
Mário César Silva <mcgato33@hotmail.com>
2020-10-15 05:23:43 -0100

    finalizando interface grafica

--- front-end/src/pages/OrphanagesMap.tsx
+++ front-end/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
+
 import 'leaflet/dist/leaflet.css';
 
 
 import mapMarKerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarKerImg,
+
+  iconSize: [48, 58],
+  iconAnchor: [29, 68],
+  popupAnchor: [160, 2]
+}); 
 
 function OrphanagesMap() {
   return(
@@ -35,9 +45,21 @@ function OrphanagesMap() {
         <TileLayer 
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
         />
+
+        <Marker
+        icon={mapIcon}
+          position={[14.9364883,-23.5242391]}
+        >
+          <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+            Fundação Pantera
+            <Link to="/orphanages/1">
+              <FiArrowRight size={20} color="#fff" />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
 
-      <Link to="#" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#fff" />
       </Link>
     </div>
